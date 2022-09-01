dcb6e0d0170830297b0f8c19117b435e912732af
KSArjun18 <111218912+KSArjun18@users.noreply.github.com>
2022-09-01 14:57:59 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,10 @@
+function foo(){
+    var res=document.getElementById("main").value;
+    console.log(res);
+    var ele=document.getElementById("main1").value;
+    console.log(ele);
+    var data=document.getElementById("main2").value;
+    console.log(data);
+    var run=document.getElementById("main3").value;
+    console.log(run);
+}
